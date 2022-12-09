0af43a30e9507c3f0b7db8b4d353776e889c86e5
ykrsama <582697760.com>
2022-12-09 14:26:41 +0800

    add scripts

---
+++ cmake-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir -p build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=../ ../
+make -j8
+make install
+cd ..
--- loadLDD2-debug.sh
+++ loadLDD2-debug.sh
@@ -5,4 +5,4 @@ source /cvmfs/cepc.ihep.ac.cn/software/cepcenv/setup.sh
 #. /besfs/groups/higgs/Software/v01-17-05_slc6/init_ilcsoft_151105.sh
 PACKAGEDIR=/workfs2/cepc/xuliang/processor/GenJet
 export MARLIN_DLL=$MARLIN_DLL:${PACKAGEDIR}/FSClasser/lib/libFSClasser.so
-export MARLIN_DLL=$MARLIN_DLL:${PACKAGEDIR}/lib/libmWMeasurement.so
+export MARLIN_DLL=$MARLIN_DLL:${PACKAGEDIR}/build-debug/lib/libmWMeasurement.so
